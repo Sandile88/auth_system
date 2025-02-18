eaacb77d86080a88f81680b27fe6a022b4bc5e0b
Sandile88 <sandilep.mremi@gmail.com>
2025-02-18 20:33:41 +0200

    changed color for email templates

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -16,9 +16,8 @@ router.post("/logout", logout);
 router.post("/verify-email", verifyEmail);
 
 router.post("/forgot-password", forgotPassword);
-
+``
 router.post("/reset-password/:token", resetPassword);
-
 
 export default router;
 

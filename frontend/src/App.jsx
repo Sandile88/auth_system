c4df7dd67909411356ea90a0bc37af533cf2e959
Sandile88 <sandilep.mremi@gmail.com>
2025-03-11 23:11:10 +0200

    deleted redudant files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+//   return (
+//     <div className="min-h-screen bg-gradient-to-br 
+//     from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+//       <h1 className='text-blue-500 text-6xl'> testing</h1>
+//     </div>
+//   )
+// }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-red-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='text-3xl'>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+// export default App

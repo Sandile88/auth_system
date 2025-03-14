dd15076aa315c9ef79f82c74281a9a83f1aebb71
Sandile88 <sandilep.mremi@gmail.com>
2025-03-14 22:45:49 +0200

    added form structure

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,12 +1,18 @@
 import { motion } from 'motion/react'
-import React from 'react'
+import React, { useState } from 'react'
+import { Input } from '../components/input'
+import { User, Mail, Lock } from 'lucide-react'
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSignUp = () => {
-
+  const handleSignUp = (e) => {
+    e.preventDefault();
   }
   return (
+    
     <motion.div 
     initial={{ opacity: 0, y:20 }}
     animate={{ opacity: 1, y:0 }}
@@ -17,7 +23,25 @@ const SignUp = () => {
             Create An Account
           </h2>
 
-          <form onSubmit={handleSignUp}></form>
+          <form onSubmit={handleSignUp}>
+            <Input icon={User}
+            type ="name"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}/>
+
+            <Input icon={Mail}
+            type ="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
+
+            <Input icon={Lock}
+            type ="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
+          </form>
       </div>
     </motion.div>
   )

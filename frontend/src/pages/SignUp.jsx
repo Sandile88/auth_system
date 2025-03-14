b52b1bff1cfec981614322850a068ab61c67572d
Sandile88 <sandilep.mremi@gmail.com>
2025-03-14 22:47:06 +0200

    added div to center sign up form

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -12,38 +12,39 @@ const SignUp = () => {
     e.preventDefault();
   }
   return (
-    
-    <motion.div 
-    initial={{ opacity: 0, y:20 }}
-    animate={{ opacity: 1, y:0 }}
-    transition={{ duration: 0.5 }}
-    className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
-      <div className="p-8">
-          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-white to-sky-300 text-transparent bg-clip-text">
-            Create An Account
-          </h2>
+    <div className="min-h-screen flex items-center justify-center">
+      <motion.div 
+        initial={{ opacity: 0, y:20 }}
+        animate={{ opacity: 1, y:0 }}
+        transition={{ duration: 0.5 }}
+        className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+        <div className="p-8">
+            <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-white to-sky-300 text-transparent bg-clip-text">
+              Create An Account
+            </h2>
 
-          <form onSubmit={handleSignUp}>
-            <Input icon={User}
-            type ="name"
-            placeholder="Full Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}/>
+            <form onSubmit={handleSignUp}>
+              <Input icon={User}
+              type ="name"
+              placeholder="Full Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}/>
 
-            <Input icon={Mail}
-            type ="email"
-            placeholder="Email Address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}/>
+              <Input icon={Mail}
+              type ="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}/>
 
-            <Input icon={Lock}
-            type ="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}/>
-          </form>
-      </div>
-    </motion.div>
+              <Input icon={Lock}
+              type ="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
+            </form>
+        </div>
+      </motion.div>
+    </div>
   )
 }
 

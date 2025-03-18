12620d2c452954cfc364e3532f17135a706df217
Sandile88 <sandilep.mremi@gmail.com>
2025-03-18 19:17:44 +0200

    added email verifivation email background styling

--- frontend/src/pages/VerifyEmail.jsx
+++ frontend/src/pages/VerifyEmail.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const VerifyEmail = () => {
+  const [code, setCode] = useState(["", "", "", "", "", ""]);
+
   return (
-    <div>VerifyEmail</div>
+    <div className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+      <motion.div initial={{ opacity: 0, y:-50 }}
+    animate={{ opacity: 1, y:0 }}
+    transition={{ duration: 0.5 }}
+    className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden p-8 w-full">
+       <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-white to-sky-300 text-transparent bg-clip-text">
+          Verify Email
+        </h2>
+         <p className="text-sm text-gray-300 m-6">
+          Enter the 6-digit code sent to your email address
+        </p>
+
+      </motion.div>
+    </div>
   )
 }
  

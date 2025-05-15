6db281d011c0f3b743d6d4f3df64265e781a8610
Sandile88 <sandilep.mremi@gmail.com>
2025-05-15 19:19:13 +0200

    added functionality to code being pasted, or input. added submitting functionality as well

--- frontend/src/pages/VerifyEmail.jsx
+++ frontend/src/pages/VerifyEmail.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { motion } from 'motion/react'
 import { Input } from '../components/input'
@@ -11,19 +11,60 @@ const VerifyEmail = () => {
   const isLoading = false;
 
   const  handleChange = ( index, value) => {
+    const newCode = [...code];
+
+    // handling pasted content
+    if (value.length > 1) {
+      const pastedCode = value.slice(0, 6).split("");
+			for (let i = 0; i < 6; i++) {
+				newCode[i] = pastedCode[i] || "";
+			}
+			setCode(newCode);
+
+      
+			// Focus on the last non-empty input or the first empty one
+			const lastFilledIndex = newCode.findLastIndex((digit) => digit !== "");
+			const focusIndex = lastFilledIndex < 5 ? lastFilledIndex + 1 : 5;
+			inputRefs.current[focusIndex].focus();
+
+    } else {
+      newCode[index] = value;
+			setCode(newCode);
+
+			// Move focus to the next input field if value is entered
+			if (value && index < 5) {
+				inputRefs.current[index + 1].focus();
+			}
+    }
 
   }
 
+  // handling number deletion, focuses on previous digit space
   const handleKeyDown = (index, e) => {
-    
+    if (e.key === "Backspace" && !code[index] && index > 0) {
+			inputRefs.current[index - 1].focus();
+		}
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const verificationCode = code.join("");
+    console.log(`Verification code submitted: ${verificationCode}`)
+  }
+
+  // auto submit when all fields are filled
+  useEffect(() => {
+    if(code.every(digit => digit !== '')) {
+      handleSubmit(new Event('submit'));
+    }
+  }, [code])
 
   return (
     <div className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
       <motion.div initial={{ opacity: 0, y:-50 }}
       animate={{ opacity: 1, y:0 }}
       transition={{ duration: 0.5 }}
-      className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden p-8 w-full">
+      className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden p-8 w-full max-w-md ">
        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-white to-sky-300 text-transparent bg-clip-text">
           Verify Your Email
         </h2>
@@ -31,7 +72,7 @@ const VerifyEmail = () => {
           Enter the 6-digit code sent to your email address
         </p>
 
-         <form className="space-y-6">
+         <form onSubmit={handleSubmit} className="space-y-6">
           <div className="flex justify-between">
             {code.map((digit, index) => (
               <input

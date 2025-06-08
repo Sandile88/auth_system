efa4247a8e39bf3ae1f197c760f42056e2708f29
Sandile88 <sandilep.mremi@gmail.com>
2025-06-08 17:21:12 +0200

    added another origin in cors middleware and retrived .env contents

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config(); //to access.env values
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); //handles auth
+app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173"], credentials: true })); //handles auth
 
 app.use(express.json()); //parsing json bodies in the express app
 app.use(cookieParser()); //parsing incoming cookies

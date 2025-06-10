6b1be0171150695a40e63680781a40810cb2dcdb
Sandile88 <sandilep.mremi@gmail.com>
2025-06-10 17:49:55 +0200

    making frontend folder to be static(only during production), and rendering react application

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ import { connectDB } from "./database/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 
 dotenv.config(); //to access .env values
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173"], credentials: true })); //handles auth
 
@@ -18,6 +20,9 @@ app.use(cookieParser()); //parsing incoming cookies
 
 app.use("/api/auth", authRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+}
 
 app.listen(PORT, () => {
     connectDB();
